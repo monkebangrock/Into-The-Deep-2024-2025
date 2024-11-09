21a0da3bb2c050418a1de3cee930b0e6cba81b14
caticorn3197 <caticorn3197@gmail.com>
2024-11-09 15:37:31 -0500

    Added drivetrain controls, moved slide controls to gp2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main_2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main_2024.java
@@ -91,8 +91,18 @@ public class Main_2024 extends LinearOpMode {
         rightBackDrive = hardwareMap.get(DcMotorEx.class, "rightBack");
         slideR = hardwareMap.get(DcMotorEx.class, "slideR");
         slideL = hardwareMap.get(DcMotorEx.class, "slideL");
+        //reset encoder
+        leftFrontDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackDrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         slideR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         slideL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        //brake motors
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         slideR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         slideL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         slideTarget = 0;
@@ -108,9 +118,9 @@ public class Main_2024 extends LinearOpMode {
         // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
         // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
         // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
-        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
         slideL.setDirection(DcMotorSimple.Direction.REVERSE);
 
@@ -121,73 +131,42 @@ public class Main_2024 extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
+        leftFrontDrive.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        leftBackDrive.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        rightBackDrive.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            /*double max;
+            double y = -(gamepad1.left_stick_y); // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double rightBackPower = (y + x - rx) / denominator;
+            double leftBackPower = (y - x + rx) / denominator;
+            double leftFrontPower = (y + x + rx) / denominator;
+            double rightFrontPower = (y - x - rx) / denominator;
 
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral =  gamepad1.left_stick_x;
-            double yaw     =  gamepad1.right_stick_x;
-
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = axial + lateral + yaw;
-            double rightFrontPower = axial - lateral - yaw;
-            double leftBackPower   = axial - lateral + yaw;
-            double rightBackPower  = axial + lateral - yaw;
-
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
-
-            if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
-            }
-
-            // This is test code:
-            //
-            // Uncomment the following code to test your motor directions.
-            // Each button should make the corresponding motor run FORWARD.
-            //   1) First get all the motors to take to correct positions on the robot
-            //      by adjusting your Robot Configuration if necessary.
-            //   2) Then make sure they run in the correct direction by modifying the
-            //      the setDirection() calls above.
-            // Once the correct motors move in the correct direction re-comment this code.
-
-            /*
-            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-
-
-            // Send calculated power to wheels
-            leftFrontDrive.setPower(leftFrontPower);
-            rightFrontDrive.setPower(rightFrontPower);
-            leftBackDrive.setPower(leftBackPower);
-            rightBackDrive.setPower(rightBackPower);
+            //Drivetrain (Gamepad1 left + right joystick)
+            leftFrontDrive.setPower(-leftFrontPower*200);
+            leftBackDrive.setPower(leftBackPower*200);
+            rightFrontDrive.setPower(-rightFrontPower*200);
+            rightBackDrive.setPower(rightBackPower*200);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-            telemetry.update();*/
+            telemetry.update();
 
-            if (gamepad1.left_stick_y > 0) {
-                slide(gamepad1.left_stick_y);
+            if (gamepad2.left_stick_y > 0) {
+                slide(gamepad2.left_stick_y);
                 telemetry.addData("extend ", gamepad1.left_stick_y);
                 telemetry.addData("SlideR", slideR.getCurrentPosition());
                 telemetry.addData("SlideL", slideL.getCurrentPosition());
                 telemetry.update();
-            } else if (gamepad1.left_stick_y < 0) {
-                slide(gamepad1.left_stick_y);
+            } else if (gamepad2.left_stick_y < 0) {
+                slide(gamepad2.left_stick_y);
                 telemetry.addData("retract ", gamepad1.left_stick_y);
                 telemetry.addData("SlideR", slideR.getCurrentPosition());
                 telemetry.addData("SlideL", slideL.getCurrentPosition());
@@ -208,14 +187,14 @@ public class Main_2024 extends LinearOpMode {
             slideL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             slideR.setVelocity(5000);
             slideL.setVelocity(5000);
-            if (gamepad1.left_stick_y < 0) {
+            if (gamepad2.left_stick_y < 0) {
                 // Go UP
                 slideTarget += (int) (-slideAmount * 300);
                 if (slideTarget < 900) {
                     slideTarget = 900;
                 }
                 slideMoving = true;
-            } else if (gamepad1.left_stick_y > 0) {
+            } else if (gamepad2.left_stick_y > 0) {
                 // Go DOWN
                 slideTarget += (int) (-slideAmount * 300);
                 if (slideTarget > 0) {
