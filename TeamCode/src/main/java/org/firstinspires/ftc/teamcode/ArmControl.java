668393538c4164a20130dc5d46a09f3b4da61c25
iciclepiano18 <iciclepiano18@gmail.com>
2024-11-16 16:45:14 -0500

    Arm Control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmControl.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@TeleOp
 public class ArmControl extends LinearOpMode {
 
     @Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawControl.java
@@ -34,3 +34,4 @@ public class ClawControl extends LinearOpMode {
         }
     }
 }
+
