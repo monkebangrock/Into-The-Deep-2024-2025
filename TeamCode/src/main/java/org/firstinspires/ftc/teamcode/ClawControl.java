49d1f519e9449c184c9b65b297bf0b8d6d052a72
iciclepiano18 <iciclepiano18@gmail.com>
2024-11-16 14:39:57 -0500

    Claw Control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawControl.java
@@ -5,11 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp
+@TeleOp(name = "Claw Control", group = "Linear Opmode")
 public class ClawControl extends LinearOpMode {
+
     @Override
     public void runOpMode() {
-        // Local variables for hardware
         DcMotor motorTest = hardwareMap.get(DcMotor.class, "motorTest");
         Servo servoTest = hardwareMap.get(Servo.class, "servoTest");
 
@@ -20,20 +20,14 @@ public class ClawControl extends LinearOpMode {
 
         while (opModeIsActive()) {
             double tgtPower = -this.gamepad1.left_stick_y;
-
-            // Motor control
             motorTest.setPower(tgtPower);
 
-            // Servo control
             if (gamepad1.y) {
                 servoTest.setPosition(0.4);
             } else if (gamepad1.x) {
                 servoTest.setPosition(0.65);
             }
 
-            // Telemetry updates
-            telemetry.addData("Status", "Running");
-            telemetry.addData("Target Power", tgtPower);
             telemetry.addData("Motor Power", motorTest.getPower());
             telemetry.addData("Servo Position", servoTest.getPosition());
             telemetry.update();
