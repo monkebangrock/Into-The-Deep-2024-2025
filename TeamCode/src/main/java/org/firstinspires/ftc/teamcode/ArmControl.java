f3b7d75775028703fa7a5d82cae7e625d2de9857
iciclepiano18 <iciclepiano18@gmail.com>
2024-11-16 15:09:10 -0500

    Claw Control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmControl.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ArmControl extends LinearOpMode {
+    private Servo armServo;
+
+    @Override
+    public void runOpMode() {
+        armServo = hardwareMap.get(Servo.class, "armServo");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.dpad_up) {
+                armServo.setPosition(1.0);
+            }
+
+            else if (gamepad1.dpad_down) {
+                armServo.setPosition(0.0);
+            } else {
+                armServo.setPosition(0.5);
+            }
+
+
+            telemetry.addData("Servo Position", armServo.getPosition());
+            telemetry.update();
+        }
+    }
+}
